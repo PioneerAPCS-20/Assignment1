570213285bc6dee19fd0986a9672ce48f2c6339c
codytheking <codytheking93@gmail.com>
2020-08-20 12:40:34 -0700

    Instructions, starter code, and examples

---
+++ Examples.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Examples here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Examples
+{
+    public static void main(String[] args)
+    {
+        int length = 4;
+        int width = 7;
+        int area = length * width;
+        
+        System.out.println("Area = " + area);
+        
+        double base1 = 2.5;
+        double base2 = 3.25;
+        double height = 10.1;
+        double trapArea = (base1 + base2) * height / 2;
+        System.out.println("Trapezoid area = " + trapArea);
+    }
+}
